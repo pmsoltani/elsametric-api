4b9c90de63cc531726fa41632be191de93c09b47
Pooria Soltani <pooria.ms@gmail.com>
2019-09-16 15:57:46 +0430

    Added new functionalies to 'api_queries'. Modified 'main' to use the new features.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 
-from api_queries import authors_list
+from api_queries import authors_list, author_id_frontend
 
 app = FastAPI()
 
@@ -15,53 +15,53 @@ async def get_authors_list(inst_fid: str = None):
     # response = {'key': 'list of all authors in the db'}
     response = authors_list()
     if inst_fid:
-        response = {'key': f'list of all authors in {inst_fid}'}
+        response = {'key': f'FEATURE !READY: list of all authors in {inst_fid}'}
     return response
 
 
-@app.get('/a/{auth_fid}')
-async def get_author(auth_fid: str):
-    response = {'key': f"{auth_fid}'s info (name, rank, contact, ...)"}
+@app.get('/a/{id_frontend}')
+async def get_author(id_frontend: str):
+    response = author_id_frontend(id_frontend)
     return response
 
 
-@app.get('/a/{auth_fid}/papers')
-async def get_author(auth_fid: str):
-    response = {'key': f"all of {auth_fid}'s papers"}
+@app.get('/a/{id_frontend}/papers')
+async def get_author(id_frontend: str):
+    response = {'key': f"all of {id_frontend}'s papers"}
     return response
 
 
-@app.get('/a/{auth_fid}/trend')
-async def get_author(auth_fid: str, year: int = None):
-    response = {'key': f"{auth_fid}'s yearly papers and citations count"}
+@app.get('/a/{id_frontend}/trend')
+async def get_author(id_frontend: str, year: int = None):
+    response = {'key': f"{id_frontend}'s yearly papers and citations count"}
     if year:
         response = {
-            'key': f"{auth_fid}'s papers for the specified year: {year}"}
+            'key': f"{id_frontend}'s papers for the specified year: {year}"}
     return response
 
 
-@app.get('/a/{auth_fid}/network')
-async def get_author(auth_fid: str, co_id: str = None):
-    response = {'key': f"all of {auth_fid}'s collaborators"}
+@app.get('/a/{id_frontend}/network')
+async def get_author(id_frontend: str, co_id: str = None):
+    response = {'key': f"all of {id_frontend}'s collaborators"}
     if co_id:
         response = {
-            'key': f"all of {auth_fid}'s papers with collaborator: {co_id}"}
+            'key': f"all of {id_frontend}'s papers with collaborator: {co_id}"}
     return response
 
 
-@app.get('/a/{auth_fid}/keywords')
-async def get_author(auth_fid: str, tag: str = None):
-    response = {'key': f"all of {auth_fid}'s keywords"}
+@app.get('/a/{id_frontend}/keywords')
+async def get_author(id_frontend: str, tag: str = None):
+    response = {'key': f"all of {id_frontend}'s keywords"}
     if tag:
         response = {
-            'key': f"all of {auth_fid}'s papers with keyword: {tag}"}
+            'key': f"all of {id_frontend}'s papers with keyword: {tag}"}
     return response
 
 
-@app.get('/a/{auth_fid}/journals')
-async def get_author(auth_fid: str, rank: str = None):
-    response = {'key': f"all of {auth_fid}'s journal Qs"}
+@app.get('/a/{id_frontend}/journals')
+async def get_author(id_frontend: str, rank: str = None):
+    response = {'key': f"all of {id_frontend}'s journal Qs"}
     if rank:
         response = {
-            'key': f"all of {auth_fid}'s papers in {rank} journals"}
+            'key': f"all of {id_frontend}'s papers in {rank} journals"}
     return response
