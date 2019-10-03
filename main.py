809412fe6679cc4c868aae5da9b11e3e3e5303a8
Pooria Soltani <pooria.ms@gmail.com>
2019-10-03 15:20:41 +0330

    API fully operational, again.

--- main.py
+++ main.py
@@ -1,16 +1,32 @@
 import uvicorn
 from fastapi import FastAPI
 
-from api_queries import authors_list_frontend, get_author, get_author_papers, \
-    get_author_trend, get_author_keywords, get_author_journals, \
-    get_author_network, get_joint_papers_id
+from api_queries import \
+    initial_response, \
+    authors_list_frontend, \
+    get_author, \
+    get_author_papers, \
+    get_author_papers_year, \
+    get_author_papers_keyword, \
+    get_author_papers_q, \
+    get_author_papers_co_id, \
+    get_author_trend, \
+    get_author_keywords, \
+    get_author_qs, \
+    get_author_network
+
 
 app = FastAPI()
 
 
 @app.get('/')
 async def home():
-    return {'key': 'Visit list', 'url': 'http://127.0.0.1:8000/a/list'}
+    return initial_response
+
+
+@app.get('/a')
+async def home():
+    return initial_response
 
 
 @app.get('/a/list')
@@ -20,47 +36,44 @@ async def show_authors_list():
 
 @app.get('/a/{id_frontend}')
 async def show_author(id_frontend: str):
-    response = get_author(id_frontend)
-    return response
+    return get_author(id_frontend)
 
 
 @app.get('/a/{id_frontend}/papers')
-async def show_author(id_frontend: str, year: int = None, coID: str = None,
-                      tag: str = None, q: str = None):
-    response = get_author_papers(id_frontend)
-    return response
+async def show_author(id_frontend: str, year: int = None, keyword: str = None,
+                      q: str = None, coID: str = None,):
+    params_set = {year, keyword, q, coID}
+    if len(params_set) > 2:  # more than 1 parameter in the request
+        return initial_response
+    if year:
+        return get_author_papers_year(id_frontend, year=year)
+    if keyword:
+        return get_author_papers_keyword(id_frontend, keyword=keyword)
+    if q:
+        return get_author_papers_q(id_frontend, q=q)
+    if coID:
+        return get_author_papers_co_id(id_frontend, co_id=coID)
+    return get_author_papers(id_frontend)
 
 
 @app.get('/a/{id_frontend}/trend')
-async def show_author(id_frontend: str, year: int = None):
-    response = get_author_trend(id_frontend)
-    if year:
-        response = get_author_papers(id_frontend, year)
-    return response
-
-
-@app.get('/a/{id_frontend}/network')
-async def show_author(id_frontend: str, coID: str = None):
-    response = get_author_network(id_frontend)
-    if coID:
-        response = get_joint_papers_id(id_frontend=id_frontend, co_id=coID)
-    return response
+async def show_author(id_frontend: str):
+    return get_author_trend(id_frontend)
 
 
 @app.get('/a/{id_frontend}/keywords')
-async def show_author(id_frontend: str, tag: str = None):
-    response = get_author_keywords(id_frontend)
-    if tag:
-        response = get_author_keywords(id_frontend, keyword=tag)
-    return response
+async def show_author(id_frontend: str):
+    return get_author_keywords(id_frontend)
 
 
-@app.get('/a/{id_frontend}/journals')
-async def show_author(id_frontend: str, q: str = None):
-    response = get_author_journals(id_frontend)
-    if q:
-        response = get_author_journals(id_frontend, q)
-    return response
+@app.get('/a/{id_frontend}/qs')
+async def show_author(id_frontend: str):
+    return get_author_qs(id_frontend)
+
+
+@app.get('/a/{id_frontend}/network')
+async def show_author(id_frontend: str):
+    return get_author_network(id_frontend)
 
 
 if __name__ == "__main__":
