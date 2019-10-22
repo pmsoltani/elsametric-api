c311b1e5fa839203f440dfb33a85d59b6bd316cd
Pooria Soltani <pooria.ms@gmail.com>
2019-10-22 23:43:38 +0330

    Added several other metrics to the function. Nearly all metrics come from a single loop.

--- elsaserver/api/get_author_stats.py
+++ elsaserver/api/get_author_stats.py
@@ -3,7 +3,24 @@ from datetime import datetime
 from elsametric.models.author import Author
 from elsametric.models.country import Country
 
-# from .. import home_country
+from .. import home_country, home_institution
+
+paper_types_mapper = {
+    'ar': 'article',
+    'ip': 'article',
+    'cp': 'conference',
+    're': 'review',
+    'bk': 'book',
+    'ch': 'book',
+    'tb': 'retracted',
+    'le': 'other',
+    'ed': 'other',
+    'no': 'note',
+    'er': 'other',
+    'sh': 'other',
+    'ab': 'other',
+    'na': 'other'
+}
 
 
 def get_author_stats(session, id_backend: int):
@@ -11,20 +28,24 @@ def get_author_stats(session, id_backend: int):
     stats = {
         'hIndex': None,
         'i10Index': None,
-        'totalPapers': None,
-        'totalCitationss': None,
-        'thisYearPapers': None,
-        'thisYearCitationss': None,
-        'intl_collaborations': None,
+        'papers': {
+            'totalPapers': None,
+            'totalCitationss': None,
+            'thisYearPapers': None,
+            'thisYearCitationss': None
+        },
+        'paperTypes': {},
+        'collaborations': {
+            'singleAuthorship': None,
+            'instCollaborations': None,
+            'natCollaborations': None,
+            'intlCollaborations': None
+        }
     }
-
-    home_country = session \
-        .query(Country) \
-        .filter(Country.domain == 'IR') \
-        .first()
 
     try:
         author = session.query(Author).get(id_backend)  # None if not found
+        this_year = datetime.now().year
 
         stats['hIndex'] = {
             'value': author.h_index_gsc,  # possible AttributeError
@@ -35,30 +56,61 @@ def get_author_stats(session, id_backend: int):
             'retrievalTime': author.retrieval_time_gsc,
         }
 
-        papers_trend = author.get_papers()
-        citations_trend = author.get_citations()
-        this_year = datetime.now().year
+        total_citations = this_year_papers = this_year_citations = 0
+        single_authorship = inst_collaborations = 0
+        nat_collaborations = intl_collaborations = 0
 
-        stats['totalPapers'] = {'value': sum(papers_trend.values())}
-        stats['totalCitationss'] = {'value': sum(citations_trend.values())}
-        if this_year in papers_trend.keys():
-            stats['thisYearPapers'] = {'value': papers_trend[this_year]}
-            stats['thisYearCitationss'] = {'value': citations_trend[this_year]}
-
-        intl_collaborations = 0
         papers = [paper_author.paper for paper_author in author.papers]
+        stats['papers']['totalPapers'] = len(papers)
         for paper in papers:
+            # papers & citations count
+            if paper.get_year() == this_year:
+                this_year_papers += 1
+                this_year_citations += paper.cited_cnt or 0
+            total_citations += paper.cited_cnt or 0
+
+            # paper types
+            paper_type = paper_types_mapper[paper.type]
+            try:
+                stats['paperTypes'][paper_type] += 1
+            except KeyError:
+                stats['paperTypes'][paper_type] = 1
+
             co_authors = [
                 paper_author.author for paper_author in paper.authors]
+            if len(co_authors) == 1:
+                single_authorship += 1
+                continue
+            is_intl_paper = is_nat_paper = False
             for co_author in co_authors:
-                if co_author == author:
+                if co_author == author:  # skipping the main author
                     continue
 
-                countries = co_author.get_countries()
-                if countries and home_country not in countries:
-                    intl_collaborations += 1
+                # Since multiple SQLAlchemy sessions might be involved (between
+                # 'home_country', 'home_institution', and 'co_author' objects),
+                # it's best to use the objects' ids in comparisons.
+                country_ids = {c.id for c in co_author.get_countries()}
+                if country_ids and home_country.id not in country_ids:
+                    is_intl_paper = True
                     break
-        stats['intl_collaborations'] = intl_collaborations
+                institution_ids = {i.id for i in co_author.get_institutions()}
+                if home_institution.id not in institution_ids:
+                    is_nat_paper = True
+
+            if is_intl_paper:
+                intl_collaborations += 1
+            elif is_nat_paper:
+                nat_collaborations += 1
+            else:
+                inst_collaborations += 1
+
+        stats['papers']['totalCitationss'] = total_citations
+        stats['papers']['thisYearPapers'] = this_year_papers
+        stats['papers']['thisYearCitationss'] = this_year_citations
+        stats['collaborations']['singleAuthorship'] = single_authorship
+        stats['collaborations']['instCollaborations'] = inst_collaborations
+        stats['collaborations']['natCollaborations'] = nat_collaborations
+        stats['collaborations']['intlCollaborations'] = intl_collaborations
 
         response = stats
     except AttributeError:
